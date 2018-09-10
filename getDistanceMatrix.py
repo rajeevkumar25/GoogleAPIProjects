8271a9b04b6cd8a935ecb220ddf148a87ebaaa09
rajeev <rajeevkumar2509@gmail.com>
2018-09-11 01:43:41 +0530

    Removed commented code

--- getDistanceMatrix.py
+++ getDistanceMatrix.py
@@ -6,23 +6,18 @@ import requests
 import json
 from key import google_maps_dist_matrix_api_key
 
-#sample_request='https://maps.googleapis.com/maps/api/distancematrix/json?units=indian&origins=ranchi&destinations=patna&mode=car&key=API-KEY'
-
 req_base_url='https://maps.googleapis.com/maps/api/distancematrix/json?'
 
 def getDistanceMatrix(requesturl):
     try:
         api_response=requests.get(requesturl)
         api_res_json=api_response.json()
-        #print(api_res_json)       
+        
         if api_res_json['status']=='OK':
-            #print(api_res_json['rows'])
             details=api_res_json['rows']
             
             for detail in details:
-                #print(detail['elements'][0].text)
                 actual_detail=detail['elements'][0]
-                
                 print('Duration:- '+actual_detail['duration']['text'])
                 print('Distance:- '+actual_detail['distance']['text'])
             
@@ -41,5 +36,5 @@ if __name__=='__main__':
         getDistanceMatrix(request_url)
     else:
         print('Not a valid search!')
-    #print(request_url)
+    
 
